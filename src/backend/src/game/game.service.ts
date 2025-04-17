37faff83664dedefe7c361bf0197687a9ded890a
Edmund Luong <edmundluong@gmail.com>
2025-04-16 17:41:32 -0700

    refactor with better typing

--- src/backend/src/game/game.service.ts
+++ src/backend/src/game/game.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+interface Card {
+  rank: number | null;
+  suit: string | null;
+}
+
+enum CardSuit {
+  Hearts = `hearts`,
+  Diamonds = `diamonds`,
+  Clubs = `clubs`,
+  Spades = `spades`,
+}
+
 @Injectable()
 export class GameService {
   getHello(): string {
@@ -7,18 +19,17 @@ export class GameService {
   }
   startGame(): string {
     // load a deck of cards
-    const deck: object[] = [];
-    const suits = ['hearts', 'diamonds', 'clovers', 'spades'];
+    const deck: Card[] = [];
     for (let i = 1; i <= 13; i++) {
-      suits.forEach((suit) => {
-        const card: { [key: string]: number | null | string } = {
+      for (const suit in CardSuit) {
+        const card: Card = {
           rank: null,
           suit: null,
         };
         card.rank = i;
         card.suit = suit;
         deck.push(card);
-      });
+      }
     }
     console.log(deck);
     return deck.length.toString();
