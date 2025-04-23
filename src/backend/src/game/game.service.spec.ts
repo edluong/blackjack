8c9dd715d42b2a6977fc789908ff3a7229e247ba
Edmund Luong <edmundluong@gmail.com>
2025-04-23 15:53:44 -0700

    add more tests for load deck

--- src/backend/src/game/game.service.spec.ts
+++ src/backend/src/game/game.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameService } from './game.service';
+import { CardSuit } from 'src/constants/constants';
 
 describe('GameService', () => {
   let service: GameService;
@@ -20,6 +21,24 @@ describe('GameService', () => {
     it('loaded deck has 52 cards', () => {
       const deck = service.loadDeck();
       expect(deck.length).toBe(52);
+    });
+    it('loaded deck only has 4 different suits', () => {
+      const deck = service.loadDeck();
+      deck.forEach((card) => {
+        expect(Object.keys(CardSuit)).toContain(card.suit);
+      });
+    });
+    it('loaded deck only has 1 to 13 rank', () => {
+      const deck = service.loadDeck();
+      const rankSet: Set<number> = new Set();
+      deck.forEach((card) => {
+        if (card.rank) {
+          rankSet.add(card.rank);
+        }
+      });
+      for (let i = 1; i <= 13; i++) {
+        expect(rankSet.has(i)).toBe(true);
+      }
     });
   });
 });
