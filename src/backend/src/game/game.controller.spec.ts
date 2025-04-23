27809ee0d0dffc372bdc1b56f60ac0dc810c7ed8
Edmund Luong <edmundluong@gmail.com>
2025-04-22 17:38:17 -0700

    add gameservice to controller test

--- src/backend/src/game/game.controller.spec.ts
+++ src/backend/src/game/game.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 describe('GameController', () => {
   let controller: GameController;
+  let service: GameService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GameController],
+      providers: [GameService],
     }).compile();
 
     controller = module.get<GameController>(GameController);
+    service = module.get<GameService>(GameService);
   });
 
   it('should be defined', () => {
