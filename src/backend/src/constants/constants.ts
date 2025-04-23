70ad3018b91cc59065ef5030bbe51b2221407572
Edmund Luong <edmundluong@gmail.com>
2025-04-22 17:09:05 -0700

    fix typo

--- src/backend/src/constants/constants.ts
+++ src/backend/src/constants/constants.ts
@@ -1,4 +1,4 @@
-// Note: enums will FAIL is stored in .d.ts files
+// Note: enums will FAIL if stored in .d.ts files
 // relevant stackoverflow article
 // https://stackoverflow.com/questions/63701326/exporting-enum-in-d-ts-leads-to-cant-resolve-error
 export enum CardSuit {
