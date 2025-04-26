beb3e5ef856ec33494584e7ce9c30ca2947ef4d9
Edmund Luong <edmundluong@gmail.com>
2025-04-26 15:39:58 -0700

    add controller test

--- src/backend/src/game/game.controller.spec.ts
+++ src/backend/src/game/game.controller.spec.ts
@@ -1,23 +1,63 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 
+const mockGameService = () => ({
+  startGame: jest.fn(),
+});
+
 describe('GameController', () => {
   let controller: GameController;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let service: GameService;
+  let service: jest.Mocked<GameService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GameController],
-      providers: [GameService],
+      providers: [
+        {
+          provide: GameService,
+          useFactory: mockGameService,
+        },
+      ],
     }).compile();
 
     controller = module.get<GameController>(GameController);
-    service = module.get<GameService>(GameService);
+    service = module.get(GameService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('startGame', () => {
+    const newGame = {
+      'gameId': 'WQkWRB42',
+      'playerHand': [
+        {
+          'rank': 7,
+          'suit': 'Spades'
+        },
+        {
+          'rank': 6,
+          'suit': 'Diamonds'
+        },
+      ],
+      'dealerHand': [
+        {
+          'rank': 3,
+          'suit': 'Hearts'
+        },
+        {
+          'rank': 5,
+          'suit': 'Spades',
+        },
+      ],
+    };
+    it('should return dealer and player hand', () => {
+      service.startGame.mockReturnValue(newGame);
+      const result = controller.startGame({});
+      expect(result).toBe(newGame);
+    });
   });
 });
