dd2979095cafcb3f16b8d275d0e25f0adde81970
Edmund Luong <edmundluong@gmail.com>
2025-04-26 15:05:27 -0700

    e2e test working

--- src/backend/test/app.e2e-spec.ts
+++ src/backend/test/app.e2e-spec.ts
@@ -3,9 +3,15 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import TestAgent from 'supertest/lib/agent';
+import * as session from 'express-session';
+import { config } from 'dotenv';
+
+config();
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let agent: TestAgent;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,10 +19,32 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    const sessionSecret = process.env.SESSION_SECRET;
+
+    if (!sessionSecret) {
+      throw new Error('SESSION_SECRET is not defined in .env');
+    }
+
+    //convert the session secret into a Buffer to ensure it's treated as a key
+    const secretKey = Buffer.from(sessionSecret, 'utf-8');
+
+    // Mock session middleware
+    app.use(
+      session({
+        secret: secretKey,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+          maxAge: 3600000, // 1 hour session
+        },
+      }),
+    );
     await app.init();
+    agent = request(app.getHttpServer());
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/game/start').expect(200);
+  it('/game/start (GET)', () => {
+    return agent.get('/game/start').send({}).expect(200);
   });
 });
